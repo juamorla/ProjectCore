8b7179a02c2f105b0d223ac472cc128a27e20a38
juamorla <juanmanuelm0204@gmail.com>
2020-05-24 16:55:06 -0500

    Agregar archivos de proyecto.

---
+++ ProjectCore.Logica/BL/Tenants.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ProjectCore.Logica.BL
+{
+    public class Tenants
+    {
+        public List<Models.DB.Tenants> GetTenants(string userId)
+        {
+            DAL.Models.ProjectCoreContext _context = new DAL.Models.ProjectCoreContext();
+
+            var listTenants = (from _tenants in _context.Tenants
+                              join _aspNetUsers in _context.AspNetUsers on _tenants.Id equals _aspNetUsers.TenantId
+                              where _aspNetUsers.Id.Equals(userId)
+                              select new Models.DB.Tenants
+                              {
+                                  Id = _tenants.Id,
+                                  Name = _tenants.Name,
+                                  Plan = _tenants.Plan,
+                                  CreatedAt = _tenants.CreatedAt,
+                                  UpdatedAt = _tenants.UpdatedAt
+                              }).ToList();
+
+            return listTenants;
+
+        }
+
+        public void GetTenants()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectCore.Logica/Models/BindingModel/ProjectsBindingModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectCore.Logica.Models.BindingModel
+{
+    public class ProjectsCreateBindingModel
+    {
+        [Required(ErrorMessage = "The field Title is Required")]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "The field Title is Required")]
+        [Display(Name = "Details")]
+        public string Details { get; set; }
+
+        [Required(ErrorMessage = "The field Title is Required")]
+        [Display(Name = "Expected Completion Date")]
+        public DateTime? ExpectedCompletionDate { get; set; }
+    }
+
+        public class ProjectsEditBindingModel
+        {
+        [Required(ErrorMessage = "The field Id is Required")]
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The field Title is Required")]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "The field Title is Required")]
+        [Display(Name = "Details")]
+        public string Details { get; set; }
+
+        [Required(ErrorMessage = "The field Title is Required")]
+        [Display(Name = "Expected Completion Date")]
+        public DateTime? ExpectedCompletionDate { get; set; }
+    }
+    
+}
